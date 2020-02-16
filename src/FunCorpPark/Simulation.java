86e8938c3bd9904de02d634745edee08065a395a
Ben <ben.f.cooper@gmail.com>
2020-02-16 12:12:02 +0000

    Added ThemePark and Simulation class

--- src/FunCorpPark/Simulation.java
+++ src/FunCorpPark/Simulation.java
@@ -1,8 +1,45 @@
 package FunCorpPark;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Simulation {
 
     public static void main(String[] args) {
 	// write your code here
     }
+
+    public ThemePark createThemePark(){
+        readAttractions();
+        readCustomers();
+
+    }
+
+    public void simulate(){
+        readTransactions();
+    }
+
+    public ArrayList<String> readFile(String fileName){
+        ArrayList<String> lines = new ArrayList<String>();
+        Scanner line = new Scanner;
+        File file = new File(fileName);
+        line.useDelimiter(System.lineSeparator());
+        while(line.hasNext()){
+            String lineInfo = line.next();
+            lines.add(lineInfo);
+        }
+        return lines;
+    }
+
+    public ArrayList<Attraction> readAttractions(){
+        ArrayList<String> list = readFile("attractions.txt");
+        for(int i = 0; i < list.size(); i++){
+            String info = list.get(i);
+            Scanner scanner = new Scanner;
+
+
+        }
+    }
+
 }
---
+++ src/FunCorpPark/ThemePark.java
@@ -0,0 +1,29 @@
+package FunCorpPark;
+
+import java.util.ArrayList;
+
+public class ThemePark {
+
+    private String name;
+    private ArrayList<Attraction> attractions;
+    private ArrayList<Customer> customers;
+
+    public ThemePark(String name, ArrayList<Attraction> attractions, ArrayList<Customer> customers){
+        this.name = name;
+        this.attractions = attractions;
+        this.customers = customers;
+    }
+
+    public float calculateTotalTransportDistance(){
+
+    }
+
+    public float calculateAverageGentleCapacity(){
+
+    }
+
+    public float calculateAverageCoasterSpeed(){
+
+    }
+
+}
